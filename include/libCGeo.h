b8e07d9357ceb8ac55737690549cbec2ed1bc2c8
jwlodek <jwlodek@cs.stonybrook.edu>
2019-02-06 16:16:23 -0500

    tested doxygen, updated config file

--- include/libCGeo.h
+++ include/libCGeo.h
@@ -14,12 +14,17 @@
 #define LIBCGEO_H
 
 
+// include files
+#include <stdio.h>
+
 
 //----------------------------------------------------------------
 // Enums and Types
 //----------------------------------------------------------------
 
-/** Enum that specifies degree of precision.
+
+/** 
+ * Enum that specifies degree of precision.
  * libCGeo supports treating points with integer precision as well as floating point precision
  */
 typedef enum CG_DTYP {
@@ -28,7 +33,8 @@ typedef enum CG_DTYP {
 } CGType_t;
 
 
-/** Enum that specifies whether the point is in 2D or 3D space.
+/** 
+ * Enum that specifies whether the point is in 2D or 3D space.
  * libCGeo supports 2D cartesian points and 3D points, though not all functions support both.
  */
 typedef enum CG_DIMS {
@@ -43,37 +49,49 @@ typedef enum CG_DIMS {
 typedef enum CG_TURN {
     CG_TURN_LEFT,   /**< Left turn */
     CG_TURN_RIGHT,  /**< Right turn */
-    CG_TURN_INLINE  /**< Inline turn, meaning that the three points are colinear */
+    CG_TURN_INLINE,  /**< Inline turn, meaning that the three points are colinear */
 } CGTurn_t;
 
+
+/**
+ * Enum that specifies the type of error encountered by libCGeo.
+ */
 typedef enum CG_ERROR {
-    CG_SUCCESS              = 0,
-    CG_INVALID_TYPE         = -1,
-    CG_INVALID_DIMS         = -2,
-    CG_POINTS_TOO_FEW       = -3
+    CG_SUCCESS              = 0,    /**< Successful completion */
+    CG_INVALID_TYPE         = -1,   /**< Invalid Point Type for operation */
+    CG_INVALID_DIMS         = -2,   /**< Invalid dimensions for operation */
+    CG_POINTS_TOO_FEW       = -3,    /**< Not enough points in set to perform operation (Usually >3) */
 } CGError_t;
+
 
 //----------------------------------------------------------------
 // Data Structures
 //----------------------------------------------------------------
 
 
-/** Struct for point in cartesian plane with floating-point coordinates.
+/** 
+ * Struct for point in cartesian plane with floating-point coordinates.
  * Contains x, y, and z coordinates. In 2D points, dims should be set to CG_2D, and the zcoord will
  * not be used.
  */
 typedef struct CG_Point {
-    double xcoord;  /**< Double x-coordinate */
-    double ycoord;  /**< Double y-coordinate */
-    double zcoord;  /**< Double z-coordinate, only used in CG_3D */
-    CGDims_t dims;   /**< Dimensions of point (does it use 2 or 3) */
-    CGType_t type;   /**< Data type used by point, int or double */
+    double xcoord;          /**< Double x-coordinate */
+    double ycoord;          /**< Double y-coordinate */
+    double zcoord;          /**< Double z-coordinate, only used in CG_3D */
+    double sort_val;        /**< Double value used for sorting points */
+    char* srot_val_desc;    /**< Description of what measurement is stored in the sort_val */
+    CGDims_t dims;          /**< Dimensions of point (does it use 2 or 3) */
+    CGType_t type;          /**< Data type used by point, int or double */
 } CGPoint_t;
 
 
+/**
+ * Struct for storing set of points.
+ * Contains pointer to array of points and a counter for the number of points
+ */
 typedef struct CG_PointSet {
-    CGPoint_t* points;
-    int num_points;
+    CGPoint_t* points;      /**< Pointer to array of points */
+    int num_points;         /**< Count of number of points */
 } CGPointSet_t;
 
 
@@ -81,7 +99,21 @@ typedef struct CG_PointSet {
 // Function Definitions - Common
 //----------------------------------------------------------------
 
+/**
+ * Simple function for printing out values in point set
+ * @param point_set Input set of points
+ * @return The appropriate error code
+ */
+CGError_t print_points(CGPointSet_t* point_set);
 
+
+/**
+ * Simple function for printing out values in point set into a file
+ * @param point_set Input set of points
+ * @param fp File pointer of file in which to write
+ * @return The appropriate error code
+ */
+CGError_t print_points_to_file(CGPointSet_t* point_set, FILE* fp);
 
 
 //----------------------------------------------------------------
