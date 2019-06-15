d5798fbfc47346df5283fbbdb09c7baa90e26ced
jwlodek <jwlodek@cs.stonybrook.edu>
2019-06-15 14:40:49 -0400

    minor edits to tests

--- test/libCGeo_tests.c
+++ test/libCGeo_tests.c
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *********************************************************************************/
+
 /**
  * Unit tests for the common functionality for libCGeo used by multiple modules.
  */
